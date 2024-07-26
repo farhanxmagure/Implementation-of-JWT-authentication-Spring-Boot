7c6375df1996bdd4ba43bb167c2c0fb76a853730
Farhan Khan <90808223+Frostyxnova@users.noreply.github.com>
2024-07-26 18:18:30 +0530

    C

---
+++ demo/src/main/java/com/magure/demo/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.magure.demo.repositories;
+
+import com.magure.demo.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
---
+++ demo/src/main/java/com/magure/demo/services/UserService.java
@@ -0,0 +1,25 @@
+package com.magure.demo.services;
+
+import com.magure.demo.entities.User;
+import com.magure.demo.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+}
